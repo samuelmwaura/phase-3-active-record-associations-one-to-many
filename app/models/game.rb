b6a8b5422360df10d540f703357ac009574a6d5d
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-27 19:21:35 +0300

    completed the lab

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,12 @@
 class Game < ActiveRecord::Base
-  
+  has_many(:reviews) #indicates that one game has many reviews and thus the reviews symbolm has to be in plural
+  #This achieves the one to many relationship and ensbles retriavl of all reviews for a game
+  #has_many goes to the owner table
+  #we hence use the reviews instance method to fetch all these reviews - naming convention
+
+  ###THIS METHOD WRITTEN CUSTOMARY##
+#   def reviews
+#     Review.where(game_id:self.id)  #returns an array of all the reviews associated with a  given game.
+#     #Where always returns an array
+#   end
 end

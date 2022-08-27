57cad484d3a7c22fd9d5bc936be1af90bc0aa4b2
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-28 01:12:54 +0300

    describe message

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,10 @@
 class Game < ActiveRecord::Base
+#has_many os a macro that genrates the instance method of the name passed as a symbol
   has_many(:reviews) #indicates that one game has many reviews and thus the reviews symbolm has to be in plural
   #This achieves the one to many relationship and ensbles retriavl of all reviews for a game
   #has_many goes to the owner table
   #we hence use the reviews instance method to fetch all these reviews - naming convention
+  #We can also create reviews associated to the game instance because of this macro where we pass in an instance of a gme directly
 
   ###THIS METHOD WRITTEN CUSTOMARY##
 #   def reviews
--- app/models/review.rb
+++ app/models/review.rb
@@ -6,4 +6,6 @@ class Review < ActiveRecord::Base
     belongs_to(:game)  #this is a macro that creates a instance method for fetching the owner of an instance of this class.It is inherited
     #the passed in symbol means that this instance belongs to given single game and thus has to be singular in naming
     #The belongs_to macro has to be in the table that has the foreign key i.e the owned table 
+    #We can create new reviews that are associated with a game by passing a game instance directly to the new Review instance.The game id is extracted
+    #We can also use a inbuilt method called create_game that is called on a review instance to create a game associated to the review instance.After calling this method however, save has to be called on the review instances to save it with the association
 end
